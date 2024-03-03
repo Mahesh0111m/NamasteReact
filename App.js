59b1134505f1e1c6d416ad5b7c3f1aeb616c5147
Mahesh0111m <maheshm0111mhm@gmail.com>
2024-03-03 11:30:06 +0530

    commit2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"}, 
 [React.createElement("div",{id:"child1"}, React.createElement("h1",{}, "h1 tag") , 
 React.createElement("h2",{},"h2 tag")), React.createElement("div",{id:"child1"},
